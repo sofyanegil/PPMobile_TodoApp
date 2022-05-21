7645e519a5bb9f8ab57b9584d1c8fdddd8449623
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-05-21 14:38:08 +0700

    Create CameraPictureButton

---
+++ app/src/main/java/id/ac/unpas/todoapp/ui/camera/CameraPictureButton.kt
@@ -0,0 +1,47 @@
+package id.ac.unpas.todoapp.ui.camera
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CameraPictureButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val color = if (isPressed) Color.Blue else Color.Black
+    val contentPadding = PaddingValues(if (isPressed) 8.dp else 12.dp)
+
+    OutlinedButton(
+        modifier = modifier,
+        shape = CircleShape,
+        border = BorderStroke(2.dp, Color.Black),
+        contentPadding = contentPadding,
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Black),
+        onClick = { /*TODO*/ }, enabled = false
+    ) {
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxSize(),
+            shape = CircleShape,
+            colors = ButtonDefaults.buttonColors(backgroundColor = color),
+            interactionSource = interactionSource
+        ) {
+
+        }
+    }
+}
